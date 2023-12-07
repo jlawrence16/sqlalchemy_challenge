8c5170fc15757754c23c3c9a4c7e2deeddf48cc9
[jlawrence16] <[tamhlacht@gmail.com]>
2023-12-07 18:21:00 -0500

    adding climate code and begining app code

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 # Import the dependencies.
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine(r"sqlite:///C:/Users\tamhl\OneDrive\Documents\Bootcamp\sqlalchemy_challenge\Resources\hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes.<br/>"""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+    
+if __name__ == '__main__':
+    app.run(debug=True)
